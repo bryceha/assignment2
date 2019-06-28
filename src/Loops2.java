40cf2767825deffe9cff68c9e9dfc2aa5ea552c2
bryceha <bryce.almstrum@gmail.com>
2019-06-27 17:13:35 -0700

    first commit

---
+++ src/Loops2.java
@@ -0,0 +1,47 @@
+
+public class Loops2 {
+
+	public static void main(String[] args) {
+
+		// a
+		int i = 0;
+
+		while (i <= 100) {
+			System.out.println("Even number: " + i);
+			i += 2;
+		}
+
+		// b
+		int j = 100;
+
+		while (j >= 0) {
+			System.out.println(j);
+			j -= 3;
+		}
+		
+		// c
+		for (int c = 0; c < 100; c++) {
+			if (c % 2 == 1) {
+				System.out.println(c);
+			}
+
+		}
+		// Are we encouraged to use 'i' as much as possible? A: yes, and j is next
+
+		// d
+		for (int d = 0; d <= 100; d++) {
+			if (d % 3 == 0 && d % 5 == 0) { // added brackets here for consistency
+				System.out.println("HelloWorld");
+		    } else if (d % 3 == 0 && d % 5 != 0) { 
+				System.out.println("Hello");
+			} else if (d % 3 != 0 && d % 5 == 0) {
+				System.out.println("World");
+			} else {
+				System.out.println(d);
+			}
+
+		}
+
+	} //push whole thing into repository 
+
+}
---
+++ src/assignment2App.java
@@ -0,0 +1,39 @@
+
+public class assignment2App {
+
+	public static void main(String[] args) {
+
+		int currentTemp = 100;
+		
+		boolean isHotOutside = currentTemp >= 90;
+		System.out.println("Is it hot outside? " + isHotOutside);
+		
+		int dayNumber = 7;
+		
+		boolean isWeekday = dayNumber < 6;
+		System.out.println("Is it a weekday? " + isWeekday);
+		//make this so Sunday is 1 instead?
+		
+		double costOfMilk = 3.19;
+		double moneyInWallet = 20.00;
+		int thirstLevel = 10;
+		//I'd also like a drink
+		
+		boolean shouldBuyIceCream = isHotOutside && moneyInWallet > 5;
+		System.out.println("Should I buy ice cream? " + shouldBuyIceCream);
+		
+		boolean willGoSwimming = isHotOutside && !isWeekday;
+		System.out.println("Will I go swimming? " + willGoSwimming);
+		
+		boolean isGoodDay = !isHotOutside && moneyInWallet > 0 && !isWeekday;
+		System.out.println("Is it a good day? " + isGoodDay);
+		//I changed it to "good" being NOT hot outside
+		
+		boolean willBuyMilk = isHotOutside && thirstLevel >= 3 && moneyInWallet >= 2*costOfMilk;
+		System.out.println("Will I buy milk today? " + willBuyMilk);
+		
+		
+		
+	}
+
+}
